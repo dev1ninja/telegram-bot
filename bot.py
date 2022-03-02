3beb249e2818e71029c2f8f85f93372a47694132
dev1ninja <devninja.up@gmail.com>
2022-03-02 06:28:59 -0500

    add beef

--- bot.py
+++ bot.py
@@ -7,11 +7,13 @@ chat_id = '-1001677064857'
 
 bot = telebot.TeleBot(token)
 
+# Delete webhook at first 
 def delete_webhook():
     url = "https://api.telegram.org/bot" + token + "/deleteWebhook"
     response = requests.request("POST", url, headers={}, data={})
     print(response.text)
 
+# fat code
 def fat():
     list_of_result = {'Right':'I got the info', 'Wrong':'This is not what I am looking for'}
     markup = types.InlineKeyboardMarkup()
@@ -20,7 +22,17 @@ def fat():
 
     return markup
 
+# milk code
 def milk():
+    list_of_result = {'Question1':'Can I drink milk?', 'Question2':'Facts about milk!'}
+    markup = types.InlineKeyboardMarkup()
+    for key, value in list_of_result.items():
+        markup.add(types.InlineKeyboardButton(text=value, callback_data=key))
+
+    return markup
+
+# beef code
+def beef():
     list_of_result = {'Question1':'Can I drink milk?', 'Question2':'Facts about milk!'}
     markup = types.InlineKeyboardMarkup()
     for key, value in list_of_result.items():
@@ -32,23 +44,29 @@ def milk():
 def send_message(message):
     bot.send_message(chat_id = message.chat.id, text="Howdy, how are you doing?")
 
+# receiving msg function
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    if ('milk' in message.text):
+    if ('milk' in message.text): # in case message contains a word "milk"
         bot.send_message(chat_id = message.chat.id, text="Select one", reply_markup=milk(), parse_mode="HTML")
-    if ('fat' in message.text):
+    if ('fat' in message.text): # in case message contains a word "fat"
         bot.send_message(chat_id = message.chat.id, text="A small amount of fat is an essential part of a healthy, balanced diet. Fat is a source of essential fatty acids, which the body cannot make itself. Fat helps the body absorb vitamin A, vitamin D and vitamin E. These vitamins are fat-soluble, which means they can only be absorbed with the help of fats.", reply_markup=fat(), parse_mode="HTML")
+    if ('beef' in message.text): # in case message contains a word "beef"
+        bot.send_message(chat_id = message.chat.id, text="Beef is good for you")
 
+# handling call back function
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-    if (call.data == 'Right'):
+    if (call.data == 'Right'): # when user click button "I got the info"
         bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="Thank you!")
-    if (call.data == 'Wrong'):
+    if (call.data == 'Wrong'): # when user click button "This is not what I am looking for"
         bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="We will contact you shortly!")
-    if (call.data == 'Question1'):
+    if (call.data == 'Question1'): # when user click button "Can I drink milk"
         bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="Yes")
-    if (call.data == 'Question2'):
+    if (call.data == 'Question2'): # when user click button "Facts about milk!"
         bot.send_message(chat_id = chat_id, text="Is drinking milk good for you?\nImage result for milk\nIt's packed with important nutrients like calcium, phosphorus, B vitamins, potassium and vitamin D. Plus, it's an excellent source of protein. Drinking milk and dairy products may prevent osteoporosis and bone fractures and even help you maintain a healthy weight.", parse_mode="HTML")
 
 delete_webhook()
-bot.infinity_polling()
+
+# running bot
+bot.infinity_polling() 
